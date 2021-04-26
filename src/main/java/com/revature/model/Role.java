50ec9613fa8573604264387f786b84709fb2a880
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-26 16:15:35 -0500

    Usermodel created with g/s/toSt

---
+++ src/main/java/com/revature/model/Role.java
@@ -0,0 +1,35 @@
+package com.revature.model;
+
+public class Role {
+	private int roleId;
+	private String role;
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "Role [roleId=" + roleId + ", role=" + role + "]";
+	}
+	public Role(int roleId, String role) {
+		super();
+		this.roleId = roleId;
+		this.role = role;
+	}
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
