fb9c1480d67015c87e4f518b79c60117fb30d67a
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-27 19:45:31 -0500

    finished the DoaContract

--- src/main/java/com/revature/repo/DaoContract.java
+++ src/main/java/com/revature/repo/DaoContract.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 /**
  *
- * @param <T>
- * @param <I>
+ * @param <T> Type of model
+ * @param <I> Primary Key for model
  * 
  *   This will create a contract
  */
@@ -22,6 +22,27 @@ public interface DaoContract<T, I> {
 	  * @return the instance with the same primary key
 	  */
 	 
+	 T findById(I i);
 	 
-
+	 /**
+	  * @param t the instance to update
+	  * @return the updated instance
+	  */
+	 
+	 T update(T t);
+	 
+	 /**
+	  * 
+	  * @param t the instance to create
+	  * @return the created instance
+	  */
+     
+	 T create(T t);
+	 
+	 /**
+	  * 
+	  * @param i the primary key of the instance to be removed
+	  * @return how many were removed [0 if failure, 1 if success]
+	  */
+	 int delete(I i);
 }
