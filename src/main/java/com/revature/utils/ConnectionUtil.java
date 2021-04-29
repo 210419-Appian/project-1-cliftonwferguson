c9c9ec05f9a71dfd54ac9840a4d3fda72c2bc51e
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-29 12:42:56 -0500

    successful util connection

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -1,5 +1,34 @@
 package com.revature.utils;
 
-public class ConnectionUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionUtil {
+    
+	 public static Connection getConnection() throws SQLException {
+		 
+		 try {
+			 Class.forName("org.postgresql.Driver");
+		 }catch (ClassNotFoundException e) {
+			 e.printStackTrace();
+		 }
+		 
+		    String url = "jdbc:postgresql://p1bank.cr3mdvbkdi0b.us-east-2.rds.amazonaws.com/postgres";
+			String username = "postgres"; //you can use environment variables to hide the raw values to protect this info
+			String password = "password"; //System.getenv("keyName")
+			
+			return DriverManager.getConnection(url, username, password);
+		 }
+	 
+	 public static void main(String[] args) {
+		/*
+		 * Finally block will close the try with resources.
+		 */
+		 try(Connection conn = ConnectionUtil.getConnection()) {
+			 System.out.println("Connection successful.");
+		 }catch(SQLException e) {
+			 e.printStackTrace();
+		 }
+	}
 }
