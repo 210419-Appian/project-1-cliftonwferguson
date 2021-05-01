d1a038e6d0be18b3f563b850d8b4fdf67e2dba56
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-01 18:49:36 -0500

    working on findbyall in roledaoimpl

--- src/main/java/com/revature/daos/RoleDAOImpl.java
+++ src/main/java/com/revature/daos/RoleDAOImpl.java
@@ -1,14 +1,32 @@
 package com.revature.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.model.Role;
+import com.revature.utils.ConnectionUtil;
 
 public class RoleDAOImpl implements roleDAO{
 
 	@Override
 	public List<Role> findAll() {
-		// TODO Auto-generated method stub
+		try(Connection conn = ConnectionUtil.getConnection()) {
+			
+			String sql = "Select * FROM roles;";
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			ResultSet result = statement.executeQuery(sql);
+			
+			List<Role> list = new ArrayList<>();
+			
+			while (result.next()) {
+				Role role = new Role();
+			}
+		}
 		return null;
 	}
 
