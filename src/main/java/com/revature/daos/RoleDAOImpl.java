330b9d0d57419a896c0e33e69c28cdca42385ceb
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-01 18:34:16 -0500

    deleted past dao package, created role interface/class

---
+++ src/main/java/com/revature/daos/RoleDAOImpl.java
@@ -0,0 +1,15 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import com.revature.model.Role;
+
+public class RoleDAOImpl implements roleDAO{
+
+	@Override
+	public List<Role> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/revature/daos/roleDAO.java
@@ -0,0 +1,9 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import com.revature.model.Role;
+
+public interface roleDAO {
+    List<Role> findAll();
+}
