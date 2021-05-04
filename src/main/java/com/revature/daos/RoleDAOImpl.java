83ef120524daadbfb072eb7fb7c9107160144be0
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-04 05:27:55 -0500

    list find all roles dao

--- src/main/java/com/revature/daos/RoleDAOImpl.java
+++ src/main/java/com/revature/daos/RoleDAOImpl.java
@@ -3,6 +3,7 @@ package com.revature.daos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,9 +24,29 @@ public class RoleDAOImpl implements roleDAO{
 			
 			List<Role> list = new ArrayList<>();
 			
+			/**
+			 * role_id integer PRIMARY KEY,
+               user_role varchar(30) NOT NULL unique
+			 */
+			
+			/**
+			 * private int roleId;
+	           private String role;
+			 */
+			
 			while (result.next()) {
 				Role role = new Role();
+				role.setRoleId(result.getInt("role_id"));
+				role.setRole(result.getString("user_role"));
+				
+				list.add(role);
+				
 			}
+			
+			return list;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
