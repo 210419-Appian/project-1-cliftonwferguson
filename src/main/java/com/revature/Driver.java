66c86a9fdd3bcfab58900e5cc51d57eacb3756ba
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-05 19:01:08 -0500

    initial commit

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -20,32 +20,40 @@ public class Driver {
 	private static AccountTypeService atService = new AccountTypeService();
 
 	public static void main(String[] args) {
+		
 		System.out.println("All Roles");
 		System.out.println(" ");
+		
 		List<Role> list = rService.getAllRoles();
 		
 		for(Role r : list)
 			System.out.println(r);
+		
 		System.out.println("=======================");
 		
 		System.out.println("All Users with Role Object");
 		System.out.println(" ");
+		
 		List<User> uList = uService.showUsers();
 		
 		for(User u : uList)
 		System.out.println(u);
+		
 		System.out.println("==========================");
 		
 		System.out.println("All Account Status");
 		System.out.println(" ");
+		
 		List<AccountStatus> aslist = asService.getAllStatus();
           
 		for (AccountStatus as : aslist)
 			System.out.println(as);
+		
 		 System.out.println("=========================");
 		 
 		 System.out.println("All Account Types");
 		 System.out.println(" ");
+		 
 		 List<AccountType> atlist = atService.getAllAccountTypes();
 		 
 		 for (AccountType at : atlist)
