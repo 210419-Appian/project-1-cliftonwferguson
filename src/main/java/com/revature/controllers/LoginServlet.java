fe6d98a1b059dccf1cc4452c62341788e5b1f20e
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-08 15:24:32 -0500

    servlet login test

--- src/main/java/com/revature/controllers/LoginServlet.java
+++ src/main/java/com/revature/controllers/LoginServlet.java
@@ -1,5 +1,42 @@
 package com.revature.controllers;
 
-public class LoginServlet {
-  
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.revature.model.User;
+import com.revature.model.UserDTO;
+
+public class LoginServlet extends HttpServlet{
+   User userparam = new User();
+	
+	@Override 
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		UserDTO u = new UserDTO();
+		
+		u.username = req.getParameter("userId");  // the get parameter is coming from the html.
+		u.password = req.getParameter("password");
+		
+		RequestDispatcher rd = null;
+		PrintWriter out = resp.getWriter();
+		
+		if(u.username.equals(userparam.getUsername()) || u.username.equals(userparam.getPassword()) && u.username.equals(userparam.getPassword())) {
+			HttpSession ses = req.getSession();
+			ses.setAttribute("username", u.username);
+			
+			rd = req.getRequestDispatcher("success");
+			rd.forward(req, resp);
+		}else {
+			rd = req.getRequestDispatcher("index.html");
+			rd.include(req, resp);
+			out.print("<span style='color:red; text-align:center'>Invalid Username/Password</span>");
+		}
+	}
+	
 }
---
+++ src/main/java/com/revature/controllers/SuccessServlet.java
@@ -0,0 +1,31 @@
+package com.revature.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SuccessServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		//gets a currently active session if one exists.
+		HttpSession ses = req.getSession(false);
+		
+		if (ses == null) {
+			out.print("<h1>YOU ARE NOT LOGGED IN!!!!!!!!</h1>");
+		} else {
+			String name = (String) ses.getAttribute("username");
+			out.print("<h2>Welcome "+name+", you are successfully logged in.</h2>");
+			out.print("<a href='logout'>Click here to log out.</a>");
+		}
+		
+	}
+	
+}
