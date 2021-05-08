1e4ff3c955b1948c2d4f52fae268c83d40f164a4
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-05-08 18:12:12 -0500

    implemented the doGet for accounts in frontcontrolelr

---
+++ src/main/java/com/revature/controllers/AccountController.java
@@ -0,0 +1,28 @@
+package com.revature.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.model.Account;
+import com.revature.service.AccountService;
+
+public class AccountController {
+  
+	private AccountService accService = new AccountService();
+	private ObjectMapper om = new ObjectMapper();
+	 
+	public void getAllAccounts(HttpServletResponse resp) throws IOException {
+		// Gets my avengers from the db.
+		List<Account> list = accService.getAllAccounts(); 
+		
+		String json = om.writeValueAsString(list);
+		System.out.println(json);
+		PrintWriter pw = resp.getWriter();
+		pw.print(json);
+		resp.setStatus(200);
+		}
+}
--- src/main/java/com/revature/web/FrontControllerServlet.java
+++ src/main/java/com/revature/web/FrontControllerServlet.java
@@ -2,15 +2,42 @@ package com.revature.web;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.controllers.AccountController;
+
 public class FrontControllerServlet extends HttpServlet {
 
-	   @Override
-	   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		   
-	   }
+	private String BaseURL = null;
+	private AccountController accController = new AccountController();
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		BaseURL = config.getInitParameter("BaseURL");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json");
+
+		resp.setStatus(404); // default if someone sends a malformed request.
+
+		final String URL = req.getRequestURI().replace(BaseURL, "");
+
+		System.out.println(URL);
+
+		String[] sections = URL.split("/");
+
+		System.out.println(sections);
+
+		switch (URL) {
+		case "accounts":
+			accController.getAllAccounts(resp);
+		}
+
+	}
 }
